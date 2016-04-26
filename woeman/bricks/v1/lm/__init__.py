b06dcd41ba4e00b9be6effb6402055a0bfdc766b
David Madl <git@abanbytes.eu>
2016-04-26 19:48:12 +0100

    KenLM: verify extraOptions as a dict

--- woeman/bricks/v1/lm/__init__.py
+++ woeman/bricks/v1/lm/__init__.py
@@ -19,10 +19,22 @@ class KenLM:
         """
         pass
 
-    def configure(self, ngramOrder=5, prune="0 0 1", extraOptions=""):
+    def configure(self, ngramOrder=5, prune="0 0 1", extraOptions=None):
         """
         :param ngramOrder:   n-gram order
         :param prune:        "0 0 1" means prune singleton trigrams and above
-        :param extraOptions: additional lmplz options (e.g. --discount_fallback for small corpora)
+        :param extraOptions: additional lmplz options dict (e.g. 'discount_fallback' for small corpora)
         """
+        if extraOptions is None:
+            extraOptions = {}
+
+        extra_cmd_line = ''
+        for key in extraOptions:
+            if key == 'discount_fallback':
+                extra_cmd_line += ' --discount_fallback'
+            else:
+                raise ValueError('KenLM: unsupported extraOptions key "%s"' % key)
+
+        self.extraOptions = extra_cmd_line
+        del extraOptions, extra_cmd_line  # avoid adding these to the object below. Ugly!
         Brick.configure(self)
